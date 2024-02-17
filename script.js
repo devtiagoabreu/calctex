fd812d31a9c7cea1b03a002a1c46491043355f39
Tiago Abreu <devtiagoabreu@gmail.com>
2024-02-17 17:28:57 -0300

    .

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ document.getElementById('form').addEventListener('submit', function(e) {
   e.preventDefault();
 
   const largura = parseFloat(document.getElementById('largura').value);
+  const metros = parseFloat(document.getElementById('metros').value);
   const peso = parseFloat(document.getElementById('peso').value);
   const gramatura = parseFloat(document.getElementById('gramatura').value);
   const tara = parseFloat(document.getElementById('tara').value);
@@ -9,7 +10,7 @@ document.getElementById('form').addEventListener('submit', function(e) {
   const pes = parseFloat(document.getElementById('pes').value) / 100; // Porcentagem de Poliéster
   const pue = parseFloat(document.getElementById('pue').value) / 100; // Porcentagem de Poliuretano
   const cv = parseFloat(document.getElementById('cv').value) / 100; // Porcentagem de Viscose
-
+  
   const tipos_fio = {
       "CO": co,
       "PES": pes,
@@ -19,14 +20,29 @@ document.getElementById('form').addEventListener('submit', function(e) {
 
   let resultadoHTML = '';
 
-  Object.entries(tipos_fio).forEach(([tipo, percentual]) => {
-      const peso_liquido = peso - tara;
-      const consumo_trama = ((peso_liquido * 1000) / (gramatura * largura)) * percentual;
-      const consumo_urdume = ((peso_liquido * 1000) / (gramatura * largura)) * percentual;
+  if (metros > 0) {
+    
+    Object.entries(tipos_fio).forEach(([tipo, percentual]) => {
 
-      resultadoHTML += `<p>Consumo de trama (${tipo}): ${consumo_trama.toFixed(2)} metros/kg</p>`;
-      resultadoHTML += `<p>Consumo de urdume (${tipo}): ${consumo_urdume.toFixed(2)} metros/kg</p>`;
-  });
+      const gramaturaLinear = gramatura * largura;
+      const pesoLiquidoCalculado = metros * gramaturaLinear;
+      const consumoFioPesoCalculado = pesoLiquidoCalculado * percentual;
+        
+      resultadoHTML += `<p>Peso Líquido (${tipo}): ${pesoLiquidoCalculado.toFixed(2)} kg</p>`;
+      resultadoHTML += `<p>Consumo de trama (${tipo}): ${consumoFioPesoCalculado.toFixed(2)} kg</p>`;
+        
+    });
+  }else {
+
+    const pesoLiquidoReal = peso - tara;
+    const gramaturaLinear = gramatura * largura;
+    const metrosCalculados = pesoLiquidoReal / gramaturaLinear; 
+    const consumoFioPesoReal = pesoLiquidoReal * percentual;
+
+    resultadoHTML += `<p>Metros (${tipo}): ${metrosCalculados.toFixed(2)} m</p>`;
+    resultadoHTML += `<p>Consumo de fio (${tipo}): ${consumoFioPesoReal.toFixed(2)} kg</p>`;
+    
+  }
 
   document.getElementById('resultado').innerHTML = resultadoHTML;
 });
